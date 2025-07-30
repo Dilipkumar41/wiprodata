09b8cdae386bfd239dad77aa5cd3b3fa0a93fb56
Dilipkumar41 <dilip424kumar@gmail.com>
2025-07-30 21:16:36 +0530

    Training 9

---
+++ Training9/MockExample/MockExample/DetailsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+
+namespace MockExample
+{
+    [TestFixture]
+    internal class DetailsTest
+    {
+        [Test]
+        public void TestShowStudent()
+        {
+            Mock<IDetails> mockDetails = new Mock<IDetails>();
+            mockDetails.Setup(d => d.ShowStudents()).Returns("Hi I am Kusuma...");
+            Assert.AreEqual("Hi I am Kusuma...", mockDetails.Object.ShowStudents());
+        }
+
+        [Test]
+        public void TestShowCompany()
+        {
+            Mock<IDetails> obj = new Mock<IDetails>();
+            obj.Setup(x => x.ShowCompany()).Returns("Its from Wipro Bangalore...");
+            Assert.AreEqual("Its from Wipro Bangalore...", obj.Object.ShowCompany());
+            //Details obj  = new Details();
+            //Assert.AreEqual("Its from Wipro Chennai...",obj.ShowCompany());
+        }
+    }
+}
---
+++ Training9/NUnitDemos/NUnitDemos/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NUnitDemos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NUnitDemos")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("82a24df9-9903-47a4-bab8-7aab26a9e8d5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
