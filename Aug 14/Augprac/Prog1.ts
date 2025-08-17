7ea43f36f487b700fca5dce5a84949301a92fff1
Dilipkumar41 <dilip424kumar@gmail.com>
2025-08-17 17:57:01 +0530

    React

---
+++ Aug 14/Augprac/Prog1.ts
@@ -0,0 +1,1 @@
+console.log("Welcome to Type Script");
---
+++ Aug 14/Augprac/prog16.ts
@@ -0,0 +1,6 @@
+const show = (eno : number, name : string,company : string = "Wipro") => {
+    console.log(eno + " " +name + " " +company)
+}
+
+show(1,"DilipKumar","Wipro");
+show(2,"Kalyan");
